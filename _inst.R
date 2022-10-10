f34a83e752f375d3892b1cc261dbb13f252f41f1
“Martin <mjfrigaard@gmail.com>
2022-10-10 12:28:57 -0700

    first commit

---
+++ _inst.R
@@ -0,0 +1,6 @@
+pkg_list <- c('base64enc', 'bslib', 'cachem', 'digest', 'evaluate', 'fastmap',
+  'fs', 'glue', 'highr', 'htmltools', 'jquerylib', 'jsonlite', 'knitr',
+	'magrittr', 'memoise', 'R6', 'rappdirs', 'rlang', 'rmarkdown', 'sass',
+	'stringi', 'stringr', 'tinytex', 'xfun', 'yaml')
+
+install.packages(pkg_list)
