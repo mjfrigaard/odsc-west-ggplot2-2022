626fbedd261ec4819a2e2b741504048664d36f81
“Martin <mjfrigaard@gmail.com>
2022-10-11 21:56:02 -0700

    new updates

--- _inst.R
+++ _inst.R
@@ -1,6 +1,11 @@
-pkg_list <- c('base64enc', 'bslib', 'cachem', 'digest', 'evaluate', 'fastmap',
-  'fs', 'glue', 'highr', 'htmltools', 'jquerylib', 'jsonlite', 'knitr',
-	'magrittr', 'memoise', 'R6', 'rappdirs', 'rlang', 'rmarkdown', 'sass',
-	'stringi', 'stringr', 'tinytex', 'xfun', 'yaml')
+# pkg_list <- c('base64enc', 'bslib', 'cachem', 'digest', 'evaluate', 'fastmap',
+#   'fs', 'glue', 'highr', 'htmltools', 'jquerylib', 'jsonlite', 'knitr',
+# 	'magrittr', 'memoise', 'R6', 'rappdirs', 'rlang', 'rmarkdown', 'sass',
+# 	'stringi', 'stringr', 'tinytex', 'xfun', 'yaml')
+# 
+# install.packages(pkg_list)
 
-install.packages(pkg_list)
+library(pagedown)
+library(fs)
+fs::dir_create("pdfs")
+pagedown::chrome_print(input = "https://mjfrigaard.github.io/odsc-west-ggplot2-2022/slides/slides.html#/title-slide", "odsc-west-ggplot2-2022.pdf")
