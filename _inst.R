a1eba392fb4419cb3fd72dbdb07594a7e5a77442
“Martin <mjfrigaard@gmail.com>
2022-10-12 10:12:02 -0700

    new site layout

--- _inst.R
+++ _inst.R
@@ -1,16 +1,18 @@
-# pkg_list <- c('base64enc', 'bslib', 'cachem', 'digest', 'evaluate', 'fastmap',
-#   'fs', 'glue', 'highr', 'htmltools', 'jquerylib', 'jsonlite', 'knitr',
-# 	'magrittr', 'memoise', 'R6', 'rappdirs', 'rlang', 'rmarkdown', 'sass',
-# 	'stringi', 'stringr', 'tinytex', 'xfun', 'yaml')
+pkg_list <- c('lubridate', 'ggplot2', 'pagedown', 'base64enc', 'bslib', 
+  'cachem', 'digest', 'evaluate', 'fastmap', 'gtable',
+  'fs', 'glue', 'highr', 'htmltools', 'jquerylib', 
+  'jsonlite', 'knitr', 'magrittr', 'memoise', 'R6', 
+  'rappdirs', 'rlang', 'rmarkdown', 'sass',
+	'stringi', 'stringr', 'tinytex', 'xfun', 'yaml')
 # 
-# install.packages(pkg_list)
+install.packages(pkg_list)
 
-library(pagedown)
-library(fs)
-library(stringr)
-library(lubridate)
-fs::dir_create("pdfs")
-tday <- stringr::str_replace_all(
-  string = stringr::str_remove_all(
-    lubridate::now(), ":"), " ", "-")
-pagedown::chrome_print(input = "https://mjfrigaard.github.io/odsc-west-ggplot2-2022/slides/slides.html#/title-slide", output = paste0("pdfs/", tday, "-odsc-west-ggplot2-2022.pdf"))
+# library(pagedown)
+# library(fs)
+# library(stringr)
+# library(lubridate)
+# fs::dir_create("pdfs")
+# tday <- stringr::str_replace_all(
+#   string = stringr::str_remove_all(
+#     lubridate::now(), ":"), " ", "-")
+# pagedown::chrome_print(input = "https://mjfrigaard.github.io/odsc-west-ggplot2-2022/slides/slides.html#/title-slide", output = paste0("pdfs/", tday, "-odsc-west-ggplot2-2022.pdf"))
